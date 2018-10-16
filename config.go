30bd0b6172d34c6f5b9b8c3c6bae57cbc8e65ef2
Olmo Ziarko <olmo.ziarko@intecbrussel.be>
2018-10-16 20:33:36 +0200

    set default db to notes.db

--- config.go
+++ config.go
@@ -14,7 +14,7 @@ func getConfig() config {
 	c := config{}
 	flag.StringVar(&c.address, "address", "localhost:8085", "The address that the server will listen on.")
 	flag.BoolVar(&c.init, "init", false, "Initialize a new database.")
-	flag.StringVar(&c.database, "database", "", "Path to an sqlite3 database file.")
+	flag.StringVar(&c.database, "database", "notes.db", "Path to an sqlite3 database file.")
 	flag.BoolVar(&c.dev, "dev", false, "Turn on development mode, do not use this in production.")
 	flag.Parse()
 	return c
